bdc172172fe1642fe6fdd68795fe45d32e45f064
artem1101gavrilov <artem1101gavrilov@gmail.com>
2018-07-19 12:10:19 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
-var http = require("http"); //РїРѕРґРєР»СЋС‡Р°РµС‚ http-РјРѕРґСѓР»СЊ, РєРѕС‚РѕСЂС‹Р№ РїРѕСЃС‚Р°РІР»СЏРµС‚СЃСЏ РІРјРµСЃС‚Рµ СЃ Node.js Рё РґРµР»Р°РµС‚ РµРіРѕ РґРѕСЃС‚СѓРїРЅС‹Рј С‡РµСЂРµР· РїРµСЂРµРјРµРЅРЅСѓСЋ http
+var http = require("http"); //подключает http-модуль, который поставляется вместе с Node.js и делает его доступным через переменную http
+var url = require("url");
 
-var circle = 0;
-
-/*
-  http.createServer();
-  server.listen(8888);
-  С„СѓРЅРєС†РёСЏ РІРѕР·РІСЂР°С‰Р°РµС‚ РѕР±СЉРµРєС‚, РёРјРµСЋС‰РёР№ РјРµС‚РѕРґ listen, РїСЂРёРЅРёРјР°СЋС‰РёР№ С‡РёСЃР»РѕРІРѕРµ Р·РЅР°С‡РµРЅРёРµ РїРѕСЂС‚Р° РЅР°С€РµРіРѕ HTTP-СЃРµСЂРІРµСЂР°, РєРѕС‚РѕСЂС‹Р№ РЅРµРѕР±С…РѕРґРёРјРѕ РїСЂРѕСЃР»СѓС€РёРІР°С‚СЊ.
-*/
-
-function onRequest(request, response) {
-  console.log("Request received.");
-  response.writeHead(200, {"Content-Type": "text/plain"}); //РґР»СЏ РѕС‚РїСЂР°РІРєРё HTTP-СЃС‚Р°С‚СѓСЃР° 200 Рё Content-Type РІ Р·Р°РіРѕР»РѕРІРєРµ HTTP-РѕС‚РІРµС‚Р°
-  response.write("Hello World! " + circle); //РґР»СЏ РѕС‚РїСЂР°РІРєРё С‚РµРєСЃС‚Р° В«Hello WorldВ» РІ С‚РµР»Рµ HTTP-РѕС‚РІРµС‚Р°
-  circle++;
-  response.end();
+function start(route) {
+	var circle = 0;
+	/*
+		http.createServer();
+		server.listen(8888);
+		функция возвращает объект, имеющий метод listen, принимающий числовое значение порта нашего HTTP-сервера, который необходимо прослушивать.
+	*/
+	function onRequest(request, response) {
+		var pathname = url.parse(request.url).pathname;
+		console.log("Request for " + pathname + " received.");
+		
+		route(pathname);
+		
+		response.writeHead(200, {"Content-Type": "text/plain"}); //для отправки HTTP-статуса 200 и Content-Type в заголовке HTTP-ответа
+		response.write("Hello World! " + circle); //для отправки текста «Hello World» в теле HTTP-ответа
+		circle++;
+		response.end(); //завершить наш ответ
+	}
+	http.createServer(onRequest).listen(8888);
+	console.log("Server has started.");
 }
-http.createServer(onRequest).listen(8888);
-console.log("Server has started.");
+
+exports.start = start;
