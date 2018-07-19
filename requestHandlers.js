6d999f04ba4e6aba34c21e0886255a3b7437bbcd
artem1101gavrilov <artem1101gavrilov@gmail.com>
2018-07-19 13:25:01 +0300

    Add files via upload

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,9 +1,21 @@
-function start() {
+﻿var exec = require("child_process").exec;
+var circle = 0;
+
+function start(response) {
   console.log("Request handler 'start' was called.");
+  
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("" + circle);
+    response.end();
+	
+	circle++;
 }
 
-function upload() {
+function upload(response) {
   console.log("Request handler 'upload' was called.");
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello Upload");
+  response.end();
 }
 
 exports.start = start;
--- server.js
+++ server.js
@@ -1,23 +1,13 @@
-var http = require("http"); //подключает http-модуль, который поставляется вместе с Node.js и делает его доступным через переменную http
+﻿var http = require("http"); //подключает http-модуль, который поставляется вместе с Node.js и делает его доступным через переменную http
 var url = require("url");
 
-function start(route) {
-	var circle = 0;
-	/*
-		http.createServer();
-		server.listen(8888);
-		функция возвращает объект, имеющий метод listen, принимающий числовое значение порта нашего HTTP-сервера, который необходимо прослушивать.
-	*/
+function start(route, handle) {
 	function onRequest(request, response) {
 		var pathname = url.parse(request.url).pathname;
 		console.log("Request for " + pathname + " received.");
 		
-		route(pathname);
+		route(handle, pathname, response);
 		
-		response.writeHead(200, {"Content-Type": "text/plain"}); //для отправки HTTP-статуса 200 и Content-Type в заголовке HTTP-ответа
-		response.write("Hello World! " + circle); //для отправки текста «Hello World» в теле HTTP-ответа
-		circle++;
-		response.end(); //завершить наш ответ
 	}
 	http.createServer(onRequest).listen(8888);
 	console.log("Server has started.");
